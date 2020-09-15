d8853050f1426ab6a5ba862f6e7204ca50bdcbab
JCSR <johnconnorsryan@gmail.com>
2020-09-15 16:11:54 -0400

    navbar for character, rolls, stats etc added and functional

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DiceRoll from './pages/diceroll'
+import Navlist from './pages/navlist'
 
 
 function App() {
+  const [currentPage, handlePageChange] = useState('DiceRoll')
+
+  const renderPage = () => {
+    switch(currentPage){
+      case 'Roll':
+        return <DiceRoll/>
+    }
+  }
+
   return(
 
   <div>
-    <p>hello</p>
-    <DiceRoll></DiceRoll>
+    <Navlist currentPage={currentPage} handlePageChange={handlePageChange}/>
+    <div className="Body">
+      {renderPage()}
+    </div>
   </div>
   )
 }
--- src/pages/diceroll.js
+++ src/pages/diceroll.js
@@ -1,4 +1,4 @@
-import React, {Component, useState} from 'react';
+import React from 'react';
 
 
 export default class diceRoll extends React.Component{
@@ -12,63 +12,49 @@ export default class diceRoll extends React.Component{
     };
 
     Dfour = () => {
-        const {d4roll} = this.setState;
         return this.setState({d4roll: Math.floor(Math.random() * 4) + 1})
     }
 
     Dsix = () => {
-        const {d6roll} = this.state;
         return this.setState({d6roll: Math.floor(Math.random() * 6) + 1});
     }
 
     Deight = () => {
-        const {d8roll} = this.state;
         return this.setState({d8roll: Math.floor(Math.random() * 8) + 1})
     }
 
     Dten = () => {
-        const {d10roll} = this.state;
         return this.setState({d10roll: Math.floor(Math.random() * 10) + 1})
     }
 
     Dtwelve = () => {
-        const {d12roll} = this.state;
         return this.setState({d12roll: Math.floor(Math.random() * 12) + 1})
     }
 
     Dtwenty = () => {
-        const {d20roll} = this.state;
         return this.setState({d20roll: Math.floor(Math.random() * 20) + 1})
     }
     
     render(){
-        const {d6roll} = this.state;
-        const {d4roll} = this.state;
-        const {d8roll} = this.state;
-        const {d10roll} = this.state;
-        const {d12roll} = this.state;
-        const {d20roll} = this.state;
-
+     
     return(
-        <div>
-             <button onClick={this.Dfour}>1d4</button> 
-                <p>{this.state.d4roll}</p>
-
-           <button onClick={this.Dsix}>1d6</button> 
-                <p>{this.state.d6roll}</p>
-
-            <button onClick={this.Deight}>1d8</button> 
-                <p>{this.state.d8roll}</p>
-
-            <button onClick={this.Dten}>1d10</button> 
-                <p>{this.state.d10roll}</p>
-
-            <button onClick={this.Dtwelve}>1d12</button> 
-                <p>{this.state.d12roll}</p>
-
-            <button onClick={this.Dtwenty}>1d20</button> 
-                <p>{this.state.d20roll}</p>
-                    
+        <div className='DiceRoll'>
+            <div className="buttonDiv">
+                <button onClick={this.Dfour}>1d4</button> 
+                <button onClick={this.Dsix}>1d6</button>
+                <button onClick={this.Deight}>1d8</button> 
+                <button onClick={this.Dten}>1d10</button> 
+                <button onClick={this.Dtwelve}>1d12</button> 
+                <button onClick={this.Dtwenty}>1d20</button> 
+            </div>
+            <div className="results">
+                <p>D4: {this.state.d4roll}</p>
+                <p>D6: {this.state.d6roll}</p>
+                <p>D8: {this.state.d8roll}</p>
+                <p>D10: {this.state.d10roll}</p>
+                <p>D12: {this.state.d12roll}</p>
+                <p>D20: {this.state.d20roll}</p>
+            </div>
         </div>
     )
     }
